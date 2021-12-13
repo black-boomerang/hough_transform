b3f2c4dc1aa437a715c400ff0d2541a7098a5cba
Aleksei Strashnov <alejo2000@yandex.ru>
2021-12-13 14:25:21 +0300

    added solution

---
+++ fast_hough_transform.py
@@ -0,0 +1,54 @@
+import time
+
+import numpy as np
+
+
+# @jit(nopython=True)
+def recursive_hough(image, left, right, memory_obj):
+    '''
+    :param image: исходное серое изображение с 1 каналом
+    :param left: левая граница части изображения
+    :param right: правая граница части изображения
+    :return: преобразование Хафа для заданной части изображения
+    '''
+    image_height = image.shape[0]
+    width = right - left + 1
+    transform = np.zeros((image_height, width))
+    memory_obj[0] += transform.size * 8
+    memory_obj[1] = max(memory_obj[0], memory_obj[1])
+    if right - left == 1:
+        transform[:, 0] = image[:, left]
+    else:
+        mid = right - (right - left) // 2
+        left_transform = recursive_hough(image, left, mid, memory_obj)
+        right_transform = recursive_hough(image, mid, right, memory_obj)
+        for y in range(image_height):
+            for shift in range(width):
+                transform[y, shift] = left_transform[y, shift // 2] + \
+                                      right_transform[
+                                          (y + shift // 2 + shift % 2) % image_height, shift // 2 - shift % 2]
+        memory_obj[0] -= left_transform.size * 8
+        memory_obj[0] -= right_transform.size * 8
+    return transform
+
+
+def fast_hough_transform(image):
+    '''
+    :param image: исходное серое изображение с 1 каналом
+    :return: преобразование Хафа изображения
+    '''
+    start_time = time.time()
+    image_width = image.shape[1]
+
+    # для корректной обработки в numba используем массив numpy, первый элемент которого
+    # содержит текущий размер занимаемой памяти, а второй - максимальный
+    memory_obj = np.zeros(2)
+
+    # рассматриваем как положительные, так и отрицательные углы
+    transform = recursive_hough(image, 0, image_width, memory_obj)
+    flip_transform = recursive_hough(np.flip(image, axis=1), 0, image_width, memory_obj)
+
+    algorithm_time = time.time() - start_time
+
+    return np.hstack(
+        [np.flip(transform, axis=1), flip_transform[:, 1:]]), algorithm_time * 1e9 / image.size, memory_obj[1]
---
+++ main.py
@@ -0,0 +1,79 @@
+import os
+import threading
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+from fast_hough_transform import fast_hough_transform
+
+
+def transform_all_images():
+    '''
+    Поворачивает все изображения на нужный угол, используя два варианта интерполяции
+    :return: None
+    '''
+    for i in range(1, 11):
+        gray_image = cv2.imread(os.path.join('images', f'{i}.jpg'), cv2.IMREAD_GRAYSCALE)
+        gray_image = cv2.Canny(gray_image, 150, 250)
+        height, width = gray_image.shape
+        transform, _, _ = fast_hough_transform(gray_image)
+        max_var_row = np.argmax(np.var(transform, axis=0))
+        angle = np.degrees(np.arctan((max_var_row - transform.shape[1] // 2) / gray_image.shape[1]))
+
+        original_image = cv2.imread(os.path.join('images', f'{i}.jpg'), cv2.IMREAD_COLOR)
+        center = (int(width / 2), int(height / 2))
+        rotation_matrix = cv2.getRotationMatrix2D(center, -angle, 1)
+        transformed_image = cv2.warpAffine(original_image, rotation_matrix, (width, height), flags=cv2.INTER_LINEAR)
+        cv2.imwrite(os.path.join('transformed_images', f'{i}_bilinear.jpg'), transformed_image)
+        transformed_image_2 = cv2.warpAffine(original_image, rotation_matrix, (width, height), flags=cv2.INTER_NEAREST)
+        cv2.imwrite(os.path.join('transformed_images', f'{i}_nearest.jpg'), transformed_image_2)
+
+
+def measure_time_and_memory():
+    '''
+    Замеряет зависимость времени работы алгоритма от площади изображения,
+    а также времени и требуемой памяти от длины/ширины
+    :return: None
+    '''
+    time_algo = np.zeros(120)
+    mem_algo = np.zeros(120)
+    gray_image = cv2.imread(os.path.join('images', f'5.jpg'), cv2.IMREAD_GRAYSCALE)
+    gray_image = cv2.Canny(gray_image, 150, 250)
+    sizes = np.arange(10, 1201, 10)
+    for size in sizes:
+        _, time_algo[size // 10 - 1], mem_algo[size // 10 - 1] = fast_hough_transform(gray_image[:size, :size])
+
+    plt.figure(figsize=(12, 7))
+    plt.plot(sizes ** 2, time_algo)
+    plt.title('Зависимость времени работы алгоритмов от площадь изображения')
+    plt.xlabel('Площадь изображения')
+    plt.ylabel('Время работы (мсек/мегапиксель)')
+    plt.grid()
+    plt.savefig('time_from_area.png')
+    plt.show()
+
+    plt.figure(figsize=(12, 7))
+    plt.plot(sizes, time_algo)
+    plt.title('Зависимость времени работы алгоритмов от длины/ширины')
+    plt.xlabel('N')
+    plt.ylabel('Время работы (мсек/мегапиксель)')
+    plt.grid()
+    plt.savefig('time_from_N.png')
+    plt.show()
+
+    plt.figure(figsize=(12, 7))
+    plt.plot(sizes, mem_algo)
+    plt.title('Зависимость потребляемой памяти от длины/ширины')
+    plt.xlabel('N')
+    plt.ylabel('Потребляемая память (в байтах)')
+    plt.grid()
+    plt.savefig('memory.png')
+    plt.show()
+
+
+if __name__ == '__main__':
+    # transform_all_images()
+    thread = threading.Thread(target=measure_time_and_memory)
+    thread.start()
+    thread.join()
